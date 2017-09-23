a8a7cdf0501c4e0b542e3fc1309dfac648e0daf4
Josué M. Sehnem <josue@sehnem.com>
2017-09-23 17:32:41 -0300

    Limpeza do código

--- pynmet/getdata.py
+++ pynmet/getdata.py
@@ -2,7 +2,6 @@ import base64
 import os
 import requests
 import pandas as pd
-import numpy as np
 import datetime as dt
 from bs4 import BeautifulSoup
 from io import StringIO
@@ -13,19 +12,19 @@ pg_form = 'http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?'
 pg_data = 'http://www.inmet.gov.br/sonabra/pg_downDadosCodigo_sim.php'
 
 header = ['Temperatura', 'Temperatura_max', 'Temperatura_min', 'Umidade',
-          'Umidade_max', 'Umidade_min', 'Ponto_orvalho', 
+          'Umidade_max', 'Umidade_min', 'Ponto_orvalho',
           'Ponto_orvalho_max', 'Ponto_orvalho_min', 'Pressao',
-          'Pressao_max', 'Pressao_min', 'Vento_velocidade','Vento_direcao',
+          'Pressao_max', 'Pressao_min', 'Vento_velocidade', 'Vento_direcao',
           'Vento_rajada', 'Radiacao', 'Precipitacao']
 
 pynmet_path = os.path.dirname(os.path.abspath(__file__))
 filepath = os.path.join(pynmet_path, 'data', 'estacoes.csv')
 sites = pd.read_csv(filepath, index_col='codigo',
-                    dtype={'codigo':str, 'alt':int})
-    
+                    dtype={'codigo': str, 'alt': int})
+
 
 def get_from_web(code, dia_i, dia_f):
-    
+
     decoded = base64.b64encode(code.encode('ascii')).decode()
     est = pg_form + decoded
     session = requests.session()
@@ -41,7 +40,7 @@ def get_from_web(code, dia_i, dia_f):
     data_str = data_str.replace('<br>', '\n').replace('////', '')
     data_str = data_str.replace('///', '').replace('//', '').replace('/', '')
     dados = pd.read_csv(StringIO(data_str))
-    dados[['data','hora']] = dados[['data','hora']].astype(str)
+    dados[['data', 'hora']] = dados[['data', 'hora']].astype(str)
     data = pd.to_datetime(dados['data'] + dados['hora'], format="%d%m%Y%H")
     dados.set_index(data, inplace=True)
     dados = dados.drop([' codigo_estacao', 'data', 'hora'], axis=1)
@@ -51,18 +50,19 @@ def get_from_web(code, dia_i, dia_f):
 
 
 def get_from_ldb(code, db):
-    
-    dia_f = dt.date.today().strftime("%d/%m/%Y")
+
+    fmt = "%d/%m/%Y"
+    dia_f = dt.date.today().strftime(fmt)
     db = Path(db)
     if db.is_file():
         try:
             dados = pd.read_hdf(db, code)
-            dia_i = dados.index.max().strftime('%d/%m/%Y')
+            dia_i = dados.index.max().strftime(fmt)
         except:
-            dia_i = (dt.date.today() - dt.timedelta(days=365)).strftime("%d/%m/%Y")
+            dia_i = (dt.date.today() - dt.timedelta(days=365)).strftime(fmt)
     else:
-        dia_i = (dt.date.today() - dt.timedelta(days=365)).strftime("%d/%m/%Y")
-    
+        dia_i = (dt.date.today() - dt.timedelta(days=365)).strftime(fmt)
+
     if 'dados' in locals():
         last_data = get_from_web(code, dia_i, dia_f)
         dados.append(last_data)
@@ -73,7 +73,7 @@ def get_from_ldb(code, db):
     return dados
 
 
-def update_all(db = os.getenv("HOME") + '/.inmetdb.hdf'):
+def update_all(db=os.getenv("HOME") + '/.inmetdb.hdf'):
 
     for code in sites.index:
         try:
--- pynmet/inmet.py
+++ pynmet/inmet.py
@@ -1,31 +1,31 @@
 import os
 import pandas as pd
-import datetime as dt
 from .getdata import get_from_ldb, get_from_web, update_all
 
 
 class inmet:
-    
+
     pynmet_path = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(pynmet_path, 'data', 'estacoes.csv')
-    sites = pd.read_csv(filepath, index_col='codigo', dtype={'codigo':str,
-                                                             'alt':int})
-    
+    sites = pd.read_csv(filepath, index_col='codigo', dtype={'codigo': str,
+                                                             'alt': int})
+
     header = ['Temperatura', 'Temperatura_max', 'Temperatura_min', 'Umidade',
-              'Umidade_max', 'Umidade_min', 'Ponto_orvalho', 
+              'Umidade_max', 'Umidade_min', 'Ponto_orvalho',
               'Ponto_orvalho_max', 'Ponto_orvalho_min', 'Pressao',
-              'Pressao_max', 'Pressao_min', 'Vento_velocidade','Vento_direcao',
-              'Vento_rajada', 'Radiacao', 'Precipitacao']
-    
-    unidades = {'Temperatura':'°C', 'Temperatura_max':'°C',
-                'Temperatura_min':'°C', 'Umidade':'%',
-                'Umidade_max':'%', 'Umidade_min':'%', 'Ponto_orvalho':'°C', 
-                'Ponto_orvalho_max':'°C', 'Ponto_orvalho_min':'°C',
-                'Pressao':'hPa', 'Pressao_max':'hPa', 'Pressao_min':'hPa',
-                'Vento_velocidade':'m/s','Vento_direcao':'°',
-                'Vento_rajada':'m/s', 'Radiacao':'kJ/m²', 'Precipitacao':'mm'}
-    
-    def __init__(self, code = None, db = os.getenv("HOME") + '/.inmetdb.hdf'):
+              'Pressao_max', 'Pressao_min', 'Vento_velocidade',
+              'Vento_direcao', 'Vento_rajada', 'Radiacao', 'Precipitacao']
+
+    unidades = {'Temperatura': '°C', 'Temperatura_max': '°C',
+                'Temperatura_min': '°C', 'Umidade': '%',
+                'Umidade_max': '%', 'Umidade_min': '%', 'Ponto_orvalho': '°C',
+                'Ponto_orvalho_max': '°C', 'Ponto_orvalho_min': '°C',
+                'Pressao': 'hPa', 'Pressao_max': 'hPa', 'Pressao_min': 'hPa',
+                'Vento_velocidade': 'm/s', 'Vento_direcao': '°',
+                'Vento_rajada': 'm/s', 'Radiacao': 'kJ/m²',
+                'Precipitacao': 'mm'}
+
+    def __init__(self, code=None, db=os.getenv("HOME") + '/.inmetdb.hdf'):
 
         if code in inmet.sites.index.values:
             self.code = code
@@ -34,8 +34,8 @@ class inmet:
             self.lat = inmet.sites.loc[code].lat
             self.lon = inmet.sites.loc[code].lon
             self.alt = inmet.sites.loc[code].alt
-        
+
         self.dados = get_from_ldb(code, db)
-    
+
     def plot_chuva(self):
         pass
