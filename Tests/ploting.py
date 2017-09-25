c91bdc3c37ef8f98797b69a63c18c737b95670a7
Josué M. Sehnem <josue@sehnem.com>
2017-09-24 21:25:50 -0300

    Docstrings

--- Tests/ploting.py
+++ Tests/ploting.py
@@ -1,17 +1,10 @@
 import pynmet
-import numpy as np
-import matplotlib
-import datetime as dt
-import os
-from pynmet.getdata import get_from_web
-from pathlib import Path
-import pandas as pd
 
-#%matplotlib inline
+% matplotlib inline
 
 sm = pynmet.inmet('A809')
+#sm.resample('1m')
 
-sm.dados.Radiacao['2017/09/23'].plot()
 
 
 #sm.resample('1m')
