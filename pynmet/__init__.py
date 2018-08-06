df1a8ab5ecf7ce323031ffb8fb3a2979d751c821
Josué M. Sehnem <josue@sehnem.com>
2018-08-05 23:03:44 -0300

    New data structure

--- pynmet/__init__.py
+++ pynmet/__init__.py
@@ -1,4 +1,4 @@
-from .inmet import inmet
+from .inmet import sites, inmet
 from .getdata import update_all
 
 from ._version import get_versions
--- pynmet/inmet.py
+++ pynmet/inmet.py
@@ -11,65 +11,65 @@ from .getdata import get_data
 from .calculations import avg_wind
 
 
-class inmet:
+header = ['Temperatura', 'Temperatura_max', 'Temperatura_min', 'Umidade',
+          'Umidade_max', 'Umidade_min', 'Ponto_orvalho',
+          'Ponto_orvalho_max', 'Ponto_orvalho_min', 'Pressao',
+          'Pressao_max', 'Pressao_min', 'Vento_velocidade',
+          'Vento_direcao', 'Vento_rajada', 'Radiacao', 'Precipitacao']
 
-    """
-    Classe que agrupa dados e prâmetros de um estação do inmet.
-    Parametros
-    ----------
-    code : string
-        O código da estação do INMET, ex: 'A803'
-    db : string, default $HOME/.inmetdb.hdf
-        Banco de dados local utilizado para armazenamento de dados
-    """
-    pynmet_path = os.path.dirname(os.path.abspath(__file__))
-    filepath = os.path.join(pynmet_path, 'data', 'estacoes.csv')
-    sites = pd.read_csv(filepath, index_col='codigo', dtype={'codigo': str,
+unidades = {'Temperatura': '°C', 'Temperatura_max': '°C',
+            'Temperatura_min': '°C', 'Umidade': '%',
+            'Umidade_max': '%', 'Umidade_min': '%', 'Ponto_orvalho': '°C',
+            'Ponto_orvalho_max': '°C', 'Ponto_orvalho_min': '°C',
+            'Pressao': 'hPa', 'Pressao_max': 'hPa', 'Pressao_min': 'hPa',
+            'Vento_velocidade': 'm/s', 'Vento_direcao': '°',
+            'Vento_rajada': 'm/s', 'Radiacao': 'kJ/m²',
+            'Precipitacao': 'mm'}
+
+
+pynmet_path = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(pynmet_path, 'data', 'estacoes.csv')
+sites = pd.read_csv(filepath, index_col='codigo', dtype={'codigo': str,
                                                              'alt': int})
 
-    header = ['Temperatura', 'Temperatura_max', 'Temperatura_min', 'Umidade',
-              'Umidade_max', 'Umidade_min', 'Ponto_orvalho',
-              'Ponto_orvalho_max', 'Ponto_orvalho_min', 'Pressao',
-              'Pressao_max', 'Pressao_min', 'Vento_velocidade',
-              'Vento_direcao', 'Vento_rajada', 'Radiacao', 'Precipitacao']
 
-    unidades = {'Temperatura': '°C', 'Temperatura_max': '°C',
-                'Temperatura_min': '°C', 'Umidade': '%',
-                'Umidade_max': '%', 'Umidade_min': '%', 'Ponto_orvalho': '°C',
-                'Ponto_orvalho_max': '°C', 'Ponto_orvalho_min': '°C',
-                'Pressao': 'hPa', 'Pressao_max': 'hPa', 'Pressao_min': 'hPa',
-                'Vento_velocidade': 'm/s', 'Vento_direcao': '°',
-                'Vento_rajada': 'm/s', 'Radiacao': 'kJ/m²',
-                'Precipitacao': 'mm'}
+def inmet(code, local=False):
+    if code in sites.index.values:
+        df = get_data(code, local)
+        df.code = code
+        df.cod_OMM = sites.loc[code].cod_OMM
+        df.inicio_operacao = sites.loc[code].inicio_operacao
+        df.lat = sites.loc[code].lat
+        df.lon = sites.loc[code].lon
+        df.alt = sites.loc[code].alt
+        df.nome = sites.loc[code].nome[:-5]
+        return df
 
-    def __init__(self, code, db=os.getenv("HOME") + '/.inmetdb.hdf',
-                 local=False):
-        if code in inmet.sites.index.values:
-            self.code = code
-            self.cod_OMM = inmet.sites.loc[code].cod_OMM
-            self.inicio_operacao = inmet.sites.loc[code].inicio_operacao
-            self.lat = inmet.sites.loc[code].lat
-            self.lon = inmet.sites.loc[code].lon
-            self.alt = inmet.sites.loc[code].alt
-        self.dados = get_data(code, local, db)
+
+@pd.api.extensions.register_dataframe_accessor("met")
+class MetFunctions(object):
+    def __init__(self, pandas_obj):
+        self._obj = pandas_obj
+
+    @property
+    def center(self):
+        # return the geographic center point of this DataFrame
+        lat = self._obj.latitude
+        lon = self._obj.longitude
+        return (float(lon.mean()), float(lat.mean()))
 
     def resample(self, periodo):
         metodos = {'Temperatura': np.mean, 'Temperatura_max': np.max,
-                   'Temperatura_min': np.min, 'Umidade': np.mean,
-                   'Umidade_max': np.max, 'Umidade_min': np.min,
-                   'Ponto_orvalho': np.mean, 'Ponto_orvalho_max': np.max,
-                   'Ponto_orvalho_min': np.min, 'Pressao': np.mean,
-                   'Pressao_max': np.max, 'Pressao_min': np.min,
-                   'Vento_velocidade': np.mean, 'Vento_direcao': avg_wind,
-                   'Vento_rajada': np.max, 'Radiacao': np.mean,
-                   'Precipitacao': np.sum}
+                    'Temperatura_min': np.min, 'Umidade': np.mean,
+                    'Umidade_max': np.max, 'Umidade_min': np.min,
+                    'Ponto_orvalho': np.mean, 'Ponto_orvalho_max': np.max,
+                    'Ponto_orvalho_min': np.min, 'Pressao': np.mean,
+                    'Pressao_max': np.max, 'Pressao_min': np.min,
+                    'Vento_velocidade': np.mean, 'Vento_direcao': avg_wind,
+                    'Vento_rajada': np.max, 'Radiacao': np.mean,
+                    'Precipitacao': np.sum}
         self.dados = self.dados.resample(periodo).agg(metodos)
 
-    def set_timezone(self, tz='America/Sao_Paulo'):
-        self.dados = self.dados.tz_convert(tz)
 
+# https://pandas.pydata.org/pandas-docs/stable/extending.html
 
-class inmet_region(object):
-
-    def __init__(self, arg):
-        self.arg = arg
